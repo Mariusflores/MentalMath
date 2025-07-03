e1da44a32f492085c8b9fb5ca0357d99efe7dacb
Mariusflores <mada060@student.kristiania.no>
2025-07-03 19:01:57 +0200

    one unit test for SettingsViewModelTest

--- app/src/test/java/com/example/mentalmath/viewmodel/SettingsViewModelTest.kt
+++ app/src/test/java/com/example/mentalmath/viewmodel/SettingsViewModelTest.kt
@@ -1,7 +1,9 @@
 package com.example.mentalmath.viewmodel
 
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.mentalmath.logic.models.Difficulty
 import com.example.mentalmath.ui.viewmodel.SettingsViewModel
+import junit.framework.TestCase.assertEquals
 import org.junit.Test
 
 class SettingsViewModelTest {
@@ -9,5 +11,9 @@ class SettingsViewModelTest {
     @Test
     fun setDifficulty_updatesState(){
         val settingsViewModel: SettingsViewModel = SettingsViewModel()
+        val difficulty = "Hard"
+        settingsViewModel.setDifficulty(difficulty)
+
+        assertEquals(settingsViewModel.difficulty.value, difficulty)
     }
 }
