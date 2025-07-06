45a48c0214a641a2e6755d4147426aecb090b033
Mariusflores <mada060@student.kristiania.no>
2025-07-06 14:30:28 +0200

    first QUizFacory test 🟢

--- app/src/test/java/com/example/mentalmath/logic/managers/QuizFactoryTest.kt
+++ app/src/test/java/com/example/mentalmath/logic/managers/QuizFactoryTest.kt
@@ -1,30 +1,37 @@
 package com.example.mentalmath.logic.managers
 
+import com.example.mentalmath.logic.models.core.Difficulty
 import com.example.mentalmath.logic.models.core.Operator
+import com.example.mentalmath.logic.models.gamemode.GameMode
+import com.example.mentalmath.logic.models.gamemode.ModeConfiguration
 import junit.framework.TestCase.assertEquals
 import junit.framework.TestCase.assertTrue
 import org.junit.Test
 
 class QuizFactoryTest {
 
+    val quizFactory: QuizFactory = QuizFactory()
+
     @Test
+    fun generateQuizByGameMode_GameModeCasual (){
 
-    fun getQuizByDifficulty_convertsStringsAndReturnsQuiz(){
-        /*
-        var operatorList: List<String> = listOf("+", "-", "×", "÷")
-        val difficulty = "Easy"
-        val length = "7"
-        var allowedOperators = Operator.OperatorConverter.toOperatorArray(operatorList)
+        val length = 10
+        var allowedOperators = Operator.entries.toTypedArray()
+        var modeConfig = ModeConfiguration(Difficulty.EASY, allowedOperators, length,
+            GameMode.Casual )
 
-        var quiz = quizProgressionManager.generateQuizByGameMode(difficulty, operatorList, length)
+
+        var quiz = quizFactory.generateQuizByGameMode(modeConfig)
         assertEquals(quiz.size, length.toInt())
         assertTrue(quiz.all { it.operator in allowedOperators })
 
-        operatorList = listOf("×", "÷")
+        val operatorList = listOf("×", "÷")
+
         allowedOperators = Operator.OperatorConverter.toOperatorArray(operatorList)
-        quiz = quizProgressionManager.generateQuizByGameMode(difficulty, operatorList, length)
+        modeConfig = ModeConfiguration(Difficulty.EASY, allowedOperators, length,
+            GameMode.Casual )
+        quiz = quizFactory.generateQuizByGameMode(modeConfig)
         assertTrue(quiz.all { it.operator in allowedOperators })
 
-     */
     }
 }
