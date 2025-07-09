bc592dfc2b1e9ec05d6e7826f47295aee171485b
Mariusflores <mada060@student.kristiania.no>
2025-07-09 16:52:23 +0200

    removed unused imports

--- app/src/main/java/com/example/mentalmath/ui/screens/ScoreScreen.kt
+++ app/src/main/java/com/example/mentalmath/ui/screens/ScoreScreen.kt
@@ -2,23 +2,17 @@ package com.example.mentalmath.ui.screens
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
-import com.example.mentalmath.logic.models.gamemode.GameMode
 import com.example.mentalmath.ui.components.scorescreen.GameSpecificScoreScreen
 import com.example.mentalmath.ui.viewmodel.QuizViewModel
-import kotlin.time.Duration
 
 @Composable
 fun ScoreScreen(
